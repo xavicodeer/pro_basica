2e3ad43a839fb6fa82b9be2bc77dac883a5fb0ed
Xavier López <xavicodeer@gmail.com>
2026-09-02 08:46:48 -0600

    completo desafio con reglas

--- Ookini/funciones.js
+++ Ookini/funciones.js
@@ -8,24 +8,45 @@
 // console.log(calcularPrecio(3,4));
 // console.log(calcularPrecio(7,1));
 
+//---
+
+// function puedeReservar(cantidad){
+//     if(cantidad <= 2){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// // ia
+// function puedoReservar(cantidad){
+//     return cantidad <= 2;
+// }
+
+
+// if(puedeReservar(1)){
+//     console.log("Reserva confirmada");
+// } else {
+//     console.log("Lo siento, maximo 2 tazas por persona");
+// }
+
+// desafio
+
+const maximoDiario = 50;
+const maximoPorPersona = 2;
+let tazasVendidas = 50;
+
 function puedeReservar(cantidad){
-    if(cantidad <= 2){
-        return true;
-    } else {
+    let disponibilidad = maximoDiario - tazasVendidas;
+    if(cantidad > maximoPorPersona){
         return false;
+    } else {
+        return cantidad <= disponibilidad;
     }
-}
-
-// ia
-function puedoReservar(cantidad){
-    return cantidad <= 2;
-}
-
+} 
 
 if(puedeReservar(1)){
     console.log("Reserva confirmada");
 } else {
-    console.log("Lo siento, maximo 2 tazas por persona");
+    console.log("Reserva denegada");
 }
-
-
