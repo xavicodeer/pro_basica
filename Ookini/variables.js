faf6b8136e91c6db2955fe08c4c4e080b8d2a46b
Xavier López <xavicodeer@gmail.com>
2026-09-01 23:06:01 -0600

    renombro html y veo variables y tipos de datos

---
+++ Ookini/variables.js
@@ -0,0 +1,17 @@
+/*let tazasReservadasHoy = 13;
+console.log("Tazas reservadas hoy: " + tazasReservadasHoy);
+
+tazasReservadasHoy = 12;
+console.log("Tazas reservadas hoy: " + tazasReservadasHoy);
+
+const precioV60 = 9;
+console.log("Precio V60: " + precioV60);*/
+
+ const nombreCafe = "Ookini";
+ const precioV60 = 5;
+ const precioKyoto = 7;
+ const precioSiphon = 8;
+ const precioPrensa = 3;
+ const maxTazasDia = 50;
+
+let tazasReservadasHoy = 1;
