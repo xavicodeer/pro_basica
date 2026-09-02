aa26bd535cf2887f658b5d8acf707494edfd808e
Xavier López <xavicodeer@gmail.com>
2026-09-01 23:40:21 -0600

    trabaje condicionales y operadores de comparacion

---
+++ Ookini/condiciones.js
@@ -0,0 +1,20 @@
+// datos de la condicion
+
+const cupoCata = 8;
+const inscritosCata = 8;
+
+// condicional de cata abierta o llena
+
+if (cupoCata > inscritosCata) {
+    console.log("Aún hay cupos, abrir reserva.");
+} else {
+    console.log("Cata llena, ofrecer lista de espera.");
+}
+
+// operadores de comparacion
+8 > 5 //true
+4 < 9 //true
+2 >= 2 //true
+1 <= 3 //true
+5 === 5 //true
+10 !== 7 //true
