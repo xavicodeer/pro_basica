55e5b48fb357e8d45fe115d7251b4bcb8474853c
Xavier López <xavicodeer@gmail.com>
2026-09-02 00:00:21 -0600

    termine el desafio de la clase

--- Ookini/condiciones.js
+++ Ookini/condiciones.js
@@ -6,9 +6,9 @@ const inscritosCata = 8;
 // condicional de cata abierta o llena
 
 if (cupoCata > inscritosCata) {
-    console.log("Aún hay cupos, abrir reserva.");
+    // console.log("Aún hay cupos, abrir reserva.");
 } else {
-    console.log("Cata llena, ofrecer lista de espera.");
+    // console.log("Cata llena, ofrecer lista de espera.");
 }
 
 // operadores de comparacion
@@ -18,3 +18,19 @@ if (cupoCata > inscritosCata) {
 1 <= 3 //true
 5 === 5 //true
 10 !== 7 //true
+
+//desafio platzi
+
+/*
+
+*/
+
+const maxTazasEspPersona = 2;
+let tazasEspPersona = 0;
+
+
+if(tazasEspPersona < maxTazasEspPersona) {
+    console.log("Puede agregar especialidad a su pedido.");
+} else {
+    console.log("Ya no puede agregar más especialidades a su pedido.");
+}
