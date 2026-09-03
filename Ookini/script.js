e1c632b8511cf03416e0762169bdba626daf21a9
Xavier López <xavicodeer@gmail.com>
2026-09-03 10:20:36 -0600

    SweetAlert2 para la alerta estilizada para error

---
+++ Ookini/script.js
@@ -0,0 +1,38 @@
+// function calcularPrecio(precioUnitario, cantidad){
+//     const total = precioUnitario * cantidad;
+//     return total;
+// }
+
+// function puedoReservar(cantidad){
+//     return cantidad <= 2;
+// }
+
+function puedoReservar(tazasDisponibles){
+    return tazasDisponibles > 0;
+}
+
+const botonReservar = document.querySelector("#boton-reservar");
+const contadorTazas = document.querySelector("#contador-tazas");
+
+botonReservar.addEventListener("click", function(){
+    // console.log("Botón de reservar clickeado");
+    const tazasActuales = Number(contadorTazas.textContent);
+
+    if(puedoReservar(tazasActuales)){
+        contadorTazas.textContent = tazasActuales - 1;
+        // console.log("Reserva registrada")
+        Swal.fire({
+        title: "Reserva registrada!",
+        text: "Se ha registrado tu reserva exitosamente!",
+        icon: "success"
+        });
+        } else {
+        botonReservar.textContent = "Sin cupos";
+        botonReservar.disabled = true;
+        Swal.fire({
+            title: "No hay mas cupos!",
+            text: "No hay reservas disponibles",
+            icon: "error"
+        });
+    }
+});
